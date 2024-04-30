8a9d41ed41b938d958561d35e080706285ba9df7
LadyMeowMix <107066742+LadyMeowMix@users.noreply.github.com>
2024-04-29 21:27:03 -0400

    updates to api and html

--- index.js
+++ index.js
@@ -2,13 +2,41 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humitidy");
+  let windspeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = "${response.data.temperature.humidity}%";
+  windspeedElement.innerHTML = "${response.data.wind.speed}km/h";
   temperatureElement.innerHTML = math.round(temperature);
+}
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[DataTransfer.getDay()];
+
+  if (minutes < 10) {
+    minutes = "0$(minutes)";
+  }
+  return "${day} ${hours} ${minutes}";
 }
 
 function searchCity(city) {
-  let apiKey = "3874833dfb0o56ea8e32c4faft57b914";
+  let apiKey = "616b14cbd38253313b3b8852fa77335d";
   let apiURL =
     "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
   axios.get(apiURL).then(refreshWeather);
