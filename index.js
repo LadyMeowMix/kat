3885873326ec1c801b4ad8f7b0a267a90c46d66a
LadyMeowMix <107066742+LadyMeowMix@users.noreply.github.com>
2024-04-29 20:46:17 -0400

    updates to js

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "3874833dfb0o56ea8e32c4faft57b914";
+  let apiURL =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
+  axios.get(apiURL).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
