3d5336413de9b3d4f3617bfc43b07adf8c0d89f4
LadyMeowMix <107066742+LadyMeowMix@users.noreply.github.com>
2024-04-29 20:22:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function handleSearchSubmit(event) {
   event.preventDefault();
-  let searchinput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector;
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
 }
 
